4f9a09677de112e0dd92f910cb894f9dc533178a
JJhuk <ly0738j@gmail.com>
2020-02-22 16:44:04 +0900

    QueueStack gogo

--- Assignment3/QueueStack.h
+++ Assignment3/QueueStack.h
@@ -1,10 +1,37 @@
 #pragma once
 #include <stack>
 #include <queue>
+using namespace std;
 namespace assignment3
 {
+	template <typename T>
 	class QueueStack
 	{
-	};
+	public:
+		QueueStack();
+		QueueStack(unsigned int maxStackSize);
+		QueueStack(const QueueStack& other);
+		~QueueStack();
+		QueueStack& operator=(const QueueStack& rhs);
 
+		void Enqueue(const T& number);
+		T Peek() const;
+		T Dequeue();
+		T GetMax() const;
+		T GetMin() const;
+		double GetAverage() const;
+		T GetSum() const;
+		unsigned int GetCount() const;
+		unsigned int GetStackCount() const;
+	private:
+		queue<stack<T>> mQueueStack;
+		const unsigned int mMaxStackSize;
+		const unsigned int mNowQueueIndex;
+		void RenewSmartQueue();
+		T mMax;
+		T mMin;
+		T mSum;
+		T mAvg;
+		double mVariance;
+	};
 }
